0b85f19a84d4535e7d7adc40fa8b2c44ae72423e
陈俊嘉 <cjj19970505@live.cn>
2019-08-21 22:27:04 +0800

    add nuget bundle set

---
+++ nugetPackage/a.ps1
@@ -0,0 +1,4 @@
+.\CredentialProviderBundle\nuget.exe push -Source "RemoteXTeam" -ApiKey AzureDevOps RemoteX.Bluetooth.1.0.5.nupkg
+.\CredentialProviderBundle\nuget.exe push -Source "RemoteXTeam" -ApiKey AzureDevOps RemoteX.Bluetooth.Droid.1.0.3.nupkg
+.\CredentialProviderBundle\nuget.exe push -Source "RemoteXTeam" -ApiKey AzureDevOps RemoteX.Bluetooth.Win10.1.0.5.nupkg
+.\CredentialProviderBundle\nuget.exe push -Source "RemoteXTeam" -ApiKey AzureDevOps RemoteX.Bluetooth.Procedure.1.0.0.nupkg
